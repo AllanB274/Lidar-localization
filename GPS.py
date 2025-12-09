b3f0ecb9321908fe9f6bae160c72c10f27c83c7b
Lux459 <108336474+Lux459@users.noreply.github.com>
2025-12-09 18:06:53 +0100

    Die Luftwaffe versucht

--- GPS.py
+++ GPS.py
@@ -1,8 +1,9 @@
 #Prend la liste des coordonnées des balises dont l'ordre a déjà été trouvé, ça fait la moyenne des 3 coordonnées calculées avec chacune des balises (j'ai mis dans le référentiel où le 0,0 est dans le coin de la table)
-def GPS2(lcobal):
+def GPS(lcobal):
     mx = (-lcobal[0][0] + 3.-lcobal[1][0] - lcobal[2][0])/3
     my = (-lcobal[0][1] + 1.-lcobal[1][1] + 2.-lcobal[2][1])/3
     return (mx, my)
+
 
 
 
