8ab3dbdbbed6aca026f03d26c98d5074f4c71cb7
Ryan-Robot4 <delesalle.ryan@gmail.com>
2025-12-10 18:06:33 +0100

    Update GPS.py

--- GPS.py
+++ GPS.py
@@ -1,4 +1,6 @@
 #Prend la liste des coordonnées des balises dont l'ordre a déjà été trouvé, ça fait la moyenne des 3 coordonnées calculées avec chacune des balises (j'ai mis dans le référentiel où le 0,0 est dans le coin de la table)
+from get_amalgame import Paquet, Point
+
 def coordonnees_balises(L):
     def projeter(l,theta):
         for b in l:
@@ -14,6 +16,7 @@ def GPS(balises_proj):
     mx = (-lcobal[0][0] + 3.-lcobal[1][0] - lcobal[2][0])/3
     my = (-lcobal[0][1] + 1.-lcobal[1][1] + 2.-lcobal[2][1])/3
     return (mx, my)
+
 
 
 
