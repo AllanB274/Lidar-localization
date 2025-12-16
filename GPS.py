b873abec500d08e1c2f19486837dfc4c6f2a2e2e
Ryan-Robot4 <delesalle.ryan@gmail.com>
2025-12-16 13:31:23 +0100

    on dit GNSS !

--- GPS.py
+++ GPS.py
@@ -1,33 +1,18 @@
-
-
-from get_amalgames import Paquet, Point
+from get_amalgames import Paquet, Point,distance2
 import numpy as np
 
-def projeter_balises(L):
-    def projeter(l,theta):
-        coord=[]
-        for b in l:
-            x=b.centre.x*np.cos(theta)-b.centre.y*np.sin(theta)   #on rotationne
-            y=b.centre.y*np.cos(theta)+b.centre.x*np.sin(theta)
-            coord.append((x,y))
-        return coord
-    theta=-L[1].centre.angle        # l'angle entre la base table et la base robot
-    return projeter(L,theta)
-    
-
-
-def GPS(C):
-    mx = (-C[0][0] + 3000.-C[1][0] - C[2][0])/3
-    my = (-C[0][1] + 1000.-C[1][1] + 2000.-C[2][1])/3
-    return (mx, my)
+def GPS(L):   #liste de balises
+    (b1,b2,b3)=(L[0].centre,L[1].centre,L[2].centre)
+    theta_1=abs(b2.angle-b3.angle)
+    theta_2=abs(b1.angle-b3.angle)
+    theta_3=abs(b1.angle-b2.angle)
+    b=b1.dist
+    c=b2.dist
+    a=distance2(b1,b2)
+    alpha=np.arccos((b-c*np.cos(theta_3))/a)
+    return(b*np.cos(alpha+0.322),abs(b*np.sin(alpha+0.322)))
 
 
 
-
-
-
-
-
-
-
+            
 
