b60250e049c0733d02dac2f59e18ed23e9b49e28
Ryan-Robot4 <delesalle.ryan@gmail.com>
2026-01-07 22:37:52 +0100

    projection Allan
    nouvelle methode de projection

--- tracking.py
+++ tracking.py
@@ -25,26 +25,34 @@ def tracking(L,pos,a_balise,r,a):
     #r : epaisseur du cone
     #a_balise : angle de la balise par rapport au référentiel robot
     #a_balise_new : angle de la balise retrouvée
+    #beta : angle entre x table et robot
+    #xb1,yb1 : coordonné de la balise avant dans le repere table avec origine robot
+    #xb2,yb2 : idem avec la balise retrouvée
+    #alpha :angle pour projeter la balise dans la base table
     
     x,y=pos[0],pos[1]
     d_balise=np.sqrt(x**2+y**2)
+    
     points_filtrés=filtre_point(L,r,a,a_balise,d_balise) #on ne garde que les points qui sont dans dans le cone de recherche
     paquets=voisins(100,points_filtrés) #on forme des paquets (normalement un seul paquets devrait etre former
     paquets_tries=sorted(paquets,key=lambda x : x.nb, reverse=True) #tri les paquets en fonction de leur taille par ordre decroissant
     balise=paquets_tries[0] #la balise sera le paquets le plus gros
+    
     a_balise_new=balise.angle
     d_balise_new=balise.dist
-    alpha=a_balise_new-a_balise #angle duquel on s'est déplacé
-    delta_d=np.sqrt(d_balise_new**2+d_balise**2-2*(d_balise_new*d_balise*np.cos(alpha))) # al kashi pour trouver la distance dont on s'est deplacer
-    phi=np.arccos((-d_balise_new**2+delta_d**2+d_balise**2)/(2*delta_d*d_balise))*(alpha/abs(alpha)) # al kashi pour trouver l'angle phi et on multiplie par le signe de alpha pour signe phi
-    beta=np.arctan(y/x) #angle etre l'ax des x, la balise origine et le robot
-    theta=phi+beta
-    delta_x=-delta_d*np.cos(theta) #variation de x de déplacement
-    delta_y=-delta_d*np.sin(theta) #variation de y de déplacement
     
-    new_x=x+delta_x #nouvelle coordonnée x
-    new_y=y+delta_y #nouvelle coordonnée y
+    beta=np.arctan(y/x)
+    alpha=a_balise-a_balise_new+beta-np.pi 
     
-    return ((new_x,new_y),a_balise_new) # la position du robot et le nouvelle ange de la balise
+    xb2=d_balise_new*np.cos(alpha)
+    yb2=d_balise_new*np.sin(alpha)
+    
+    xb1=d_balise*np.cos(beta-180)
+    yb1=d_balise*np.sin(beta-180)
+    
+    delta_x, delta_y = xb1-xb2, yb1-yb2 #deplacement du robot
+    new_x, new_y = x+delta_x, y+delta_y #calcul nouvelle position
+    
+    return ((new_x,new_y),a_balise_new) # la position du robot et le nouvelle angle de la balise
 
 
