027514e192900a2db2f6e0f99a14497ccb156f7b
Ryan-Robot4 <delesalle.ryan@gmail.com>
2026-01-16 21:44:08 +0100

    2eme version pas finie

--- tracking.py
+++ tracking.py
@@ -1,5 +1,4 @@
-from GPS import voisins
-import numpy as np
+from GPS import voisins, trilateration
 
 
 def filtre_point(L,r,a,a_balise,d_balise):
@@ -12,47 +11,45 @@ def filtre_point(L,r,a,a_balise,d_balise):
                     Liste_point.append(point)
     return Liste_point
     
-
-
-def tracking(L,pos,a_balise,r,a):
-    #voir schema pour comprendre les appélation d'angle
-    
-    #L : liste de point
-    #pos : ancienne position du robot
-    #d_balise : ancienne distance de la balise
-    #d_balise_new : distance de la balise retrouvée
-    #a : ouverture du cone
-    #r : epaisseur du cone
-    #a_balise : angle de la balise par rapport au référentiel robot
-    #a_balise_new : angle de la balise retrouvée
-    #beta : angle entre x table et robot
-    #xb1,yb1 : coordonné de la balise avant dans le repere table avec origine robot
-    #xb2,yb2 : idem avec la balise retrouvée
-    #alpha :angle pour projeter la balise dans la base table
-    
-    x,y=pos[0],pos[1]
-    d_balise=np.sqrt(x**2+y**2)
-    
-    points_filtrés=filtre_point(L,r,a,a_balise,d_balise) #on ne garde que les points qui sont dans dans le cone de recherche
+def retrouver_balise(L,balise,a,r):
+    points_filtrés=filtre_point(L,r,a,balise.angle,balise.dist) #on ne garde que les points qui sont dans dans le cone de recherche
     paquets=voisins(100,points_filtrés) #on forme des paquets (normalement un seul paquets devrait etre former
     paquets_tries=sorted(paquets,key=lambda x : x.nb, reverse=True) #tri les paquets en fonction de leur taille par ordre decroissant
-    balise=paquets_tries[0] #la balise sera le paquets le plus gros
+    new_balise=paquets_tries[0] #la balise sera le paquets le plus gros
+    if "je sais pas encore quoi" :
+        return None
+
+    return new_balise
+
+
+def tracking(L,balises,a,r):
     
-    a_balise_new=balise.angle
-    d_balise_new=balise.dist
+    #L: liste de points
+    #balises : liste des anciennes balises,
+    #a : angle d'ouverture du cone
+    #r : rayon d'epaisseur du cone
     
-    beta=np.arctan(y/x)
-    alpha=a_balise-a_balise_new+beta-np.pi 
+    nouvelles_balises=[]
+    coords=[(0,0),(3000,1000),(0,2000),(-500,1200)]  #coordonnées des 4 balises
+    for b in balises:
+        L.append(retrouver_balise(L,b,a,r)) #on retrouve les balises une par une
     
-    xb2=d_balise_new*np.cos(alpha)
-    yb2=d_balise_new*np.sin(alpha)
+    for b in range(len(nouvelles_balises)): 
+        if nouvelles_balises[b] == None:         #si une balise est cachée
+            nouvelles_balises.pop(b)            # on la retire
+            coords.pop(b)                       # on retire sa coordonnées
+            x1,y1,x2,y2,x3,y3=coords[0][0],coords[0][1],coords[1][0],coords[1][1],coords[2][0],coords[2][1]
+            new_pos=trilateration(nouvelles_balises,x1,y1,x2,y2,x3,y3)
+        else :
+            new_pos=trilateration(nouvelles_balises[:-1],0,0,3000,1000,0,2000) #on utilise les trois balises principales
+    return new_pos,nouvelles_balises
+
+
+
     
-    xb1=d_balise*np.cos(beta-180)
-    yb1=d_balise*np.sin(beta-180)
     
-    delta_x, delta_y = xb1-xb2, yb1-yb2 #deplacement du robot
-    new_x, new_y = x+delta_x, y+delta_y #calcul nouvelle position
     
-    return ((new_x,new_y),a_balise_new) # la position du robot et le nouvelle angle de la balise
+    
+
 
 
